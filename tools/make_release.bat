c83a168ec8721aae5671900f9f9acf12b8811b39
gmax <gm79@list.ru>
2015-04-22 17:11:01 +0300

    Добавлен загрузчик модулей в публичную сборку клиента. Игнор рабочих файлов с профилями.

--- tools/make_release.bat
+++ tools/make_release.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set modules=modules\system.dll
+set modules=modules\system.dll modules\modules.lua
 set plugins=plugins\prompt.dll plugins\jmc3import.dll plugins\historyfilter.lua plugins\autowrap.lua plugins\statusbar.lua plugins\reconnect.lua plugins\colorgamecmd.lua
 set plugins2=plugins\tray.dll
 
