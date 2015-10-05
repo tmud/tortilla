535faa157ed0d0c1fc06e54a9619db9da18e1e51
gmax <gm79@list.ru>
2015-10-05 17:31:34 +0300

    Начальный api для модуля bass. Нужен тест и написание самого плагина. Нужно добавить возможность записи с микрофона.

--- modules/lbass/lbass.cpp
+++ modules/lbass/lbass.cpp
@@ -14,6 +14,7 @@ class BassLoader
 public:
     BassLoader() : bass_loaded(false) {}
     ~BassLoader() {}
+
     bool loadBass()
     {
         if (HIWORD(BASS_GetVersion()) != BASSVERSION)
@@ -23,6 +24,7 @@ public:
         bass_loaded = true;
         return true;
     }
+
     bool unloadBass()
     {
         if (!bass_loaded)
@@ -36,75 +38,12 @@ public:
             return error_bass(L"Can't unload bass");
         return true;
     }
-    bool play(const wchar_t* file, int volume, bool as_sample)
+
+    int load(const wchar_t* file, bool as_sample)
     {
         if (!bass_loaded)
-            return error(L"bass not initialized");
-        int index = -1;
-        for (int i=0,e=m_objects.size();i<e;++i)
-        {
-            BassObject* obj = m_objects[i];
-            if (obj && !obj->getname().find(file) && obj->issample() == as_sample && !obj->isplaying())
-                { index = i; break; }
-        }
-        if (index == -1)
-        {
-            index = load(file);
-            if (index == -1)
-                return false;
-        }
-        BassObject *obj = m_objects[index];
-        if (!obj->play(volume_ToFloat(volume)))
-            return error(obj->geterror().c_str());
-        return true;
-    }
+            { error(L"BASS not initialized"); return -1; }
 
-    void stop(const wchar_t* file)
-    {
-        for (int i=0,e=m_objects.size();i<e;++i)
-        {
-            BassObject* obj = m_objects[i];
-            if (obj && !obj->getname().find(file))
-                obj->stop();
-        }
-    }
-
-    void stopAll()
-    {
-        for (int i=0,e=m_objects.size();i<e;++i)
-            m_objects[i]->stop();
-    }
-
-    const wchar_t* getLastError() const
-    {
-        return m_error_msg.c_str();
-    }
-
-private:
-    bool error_bass(const wchar_t* error_text)
-    {
-       std::wstring msg(error_text);
-       msg.append(L" (error code: ");
-       wchar_t buffer[16];
-       msg.append(_itow(BASS_ErrorGetCode(), buffer, 10));
-       msg.append(L")");
-       return error(msg.c_str());
-    }
-    bool error(const wchar_t* error_text)
-    {
-        m_error_msg.assign(error_text);
-        return false;
-    }
-    int error_file(const wchar_t* error_text, const wchar_t* file)
-    {
-        m_error_msg.assign(error_text);
-        m_error_msg.append(L": ");
-        m_error_msg.append(file);
-        return -1;    
-    }
-
-    int load(const wchar_t* file)
-    {
         HANDLE hfile = CreateFile(file, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
         if (hfile == INVALID_HANDLE_VALUE)
             return error_file(L"Can't open file", file);
@@ -128,16 +67,18 @@ private:
             std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
             if (ext == L"mp3" || ext == L"wav" || ext == L"ogg" || ext == L"mp2" || ext == L"mp1" || ext == L"aiff")
             {
-                 correct_ext = true;
-                /* if (load_as_sample)
-                     index = _bass_loader.loadSample(filename.c_str());
+                correct_ext = true;
+                if (as_sample)
+                   index = _bass_loader.loadSample(file);
                  else
-                     index = _bass_loader.loadStream(filename.c_str());*/
+                   index = _bass_loader.loadStream(file);
             }
             else if (ext == L"mo3" || ext == L"xm" || ext == L"it" || ext == L"s3m" || ext == L"mtm" || ext == L"mod" || ext == L"umx")
             {
+                if (!as_sample)
+                    return error_file(L"Can't load as stream", file);
                  correct_ext = true;
-                 //index = _bass_loader.loadMusic(file);
+                 index = _bass_loader.loadMusic(file);
             }
          }
          if (!correct_ext)
@@ -147,6 +88,86 @@ private:
          return index;
     }
 
+    bool unload(int id)
+    {
+        BassObject* object = get(id);
+        if (!object) 
+            return error_id(id);
+        m_objects[id] = NULL;
+        delete object;
+        m_indexes.push_back(id);
+        return true;
+    }
+
+    bool play(int id, int volume)
+    {
+        if (!bass_loaded)
+            return error(L"BASS not initialized");
+        int objects = m_objects.size();
+        if (id >= 0 && id < objects)
+        {
+            BassObject *object = m_objects[id];
+            if (!object->play(volume_ToFloat(volume)))
+                return error(object->geterror().c_str());
+            return 0;
+        }
+        return error_id(id);
+    }
+
+    bool stop(int id)
+    {
+        BassObject *object = get(id);
+        if (!object)
+            return error_id(id);
+        object->stop();
+        return true;
+    }
+
+    void stopAll()
+    {
+        for (int i=0,e=m_objects.size();i<e;++i)
+            m_objects[i]->stop();
+    }
+
+    const wchar_t* getLastError() const
+    {
+        return m_error_msg.c_str();
+    }
+
+private:
+    bool error_bass(const wchar_t* error_text)
+    {
+       std::wstring msg(error_text);
+       msg.append(L" (error code: ");
+       wchar_t buffer[16];
+       msg.append(_itow(BASS_ErrorGetCode(), buffer, 10));
+       msg.append(L")");
+       return error(msg.c_str());
+    }
+
+    bool error(const wchar_t* error_text)
+    {
+        m_error_msg.assign(error_text);
+        return false;
+    }
+
+    bool error_id(int id)
+    {
+        wchar_t buffer[16];
+        _itow(id, buffer, 10);
+        m_error_msg.assign(L"Incorrect sound id: ");
+        m_error_msg.append(buffer);
+        return false;
+    }
+
+    int error_file(const wchar_t* error_text, const wchar_t* file)
+    {
+        m_error_msg.assign(error_text);
+        m_error_msg.append(L": ");
+        m_error_msg.append(file);
+        return -1;
+    }
+
     int loadStream(const wchar_t* file)
     {
         BassStream *stream = new (std::nothrow) BassStream;
@@ -154,6 +175,7 @@ private:
             { delete stream; return -1; }
         return push(stream);
     }
+
     int loadSample(const wchar_t* file)
     {
         BassSample *sample = new (std::nothrow) BassSample;
@@ -161,6 +183,7 @@ private:
             { delete sample; return -1; }
         return push(sample);
     }
+
     int loadMusic(const wchar_t* file)
     {
         BassMusic *music = new (std::nothrow) BassMusic;
@@ -168,15 +191,7 @@ private:
             { delete music; return -1; }
         return push(music);
     }
-    bool unload(int id)
-    {
-        BassObject* obj = get(id);
-        if (!obj) return false;
-        m_objects[id] = NULL;
-        delete obj;
-        m_indexes.push_back(id);
-        return true;
-    }
+
     BassObject* get(int id) const
     {
         int count = m_objects.size();
@@ -198,136 +213,70 @@ private:
     }
 } _bass_loader;
 
-/*int lbass_load_ex(lua_State *L, bool load_as_sample)
+int lbass_play(lua_State *L)
 {
-    if (lua_gettop(L) == 1 && lua_isstring(L, 1))
+    int n = lua_gettop(L);
+    if ((n == 1 && lua_isnumber(L, 1)) || 
+        (n == 2 && lua_isnumber(L, 1) && lua_isnumber(L, 2)))
     {
-        TU2W f(lua_tostring(L,1));
-        std::wstring filename(f);
-        int index = _bass_loader.find(filename.c_str());
-        if (index != -1)
-        {
-            lua_pushinteger(L, index);
-            return 1;
-        }
-
-        HANDLE file = CreateFile(filename.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-        if (file == INVALID_HANDLE_VALUE)
-            return error_file(L, "Can't open file: ", lua_tostring(L, 1), false);
-        DWORD hsize = 0;
-        DWORD size = GetFileSize(file, &hsize);
-        if (hsize != 0)
-            return error_file(L, "File is huge: ", lua_tostring(L, 1), false);
-        if (size == 0)
-            return error_file(L, "File is empty: ", lua_tostring(L, 1), false);
-        CloseHandle(file);
-
-        // get extension
-        bool correct_ext = false;
-        int pos = filename.rfind(L'.');
-        if (pos != -1)
-        {
-            std::wstring ext(filename.substr(pos+1));
-            std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
-            if (ext == L"mp3" || ext == L"wav" || ext == L"ogg" || ext == L"mp2" || ext == L"mp1" || ext == L"aiff")
-            {
-                correct_ext = true;
-                if (load_as_sample)
-                    index = _bass_loader.loadSample(filename.c_str());
-                else
-                    index = _bass_loader.loadStream(filename.c_str());
-            }
-            else if (ext == L"mo3" || ext == L"xm" || ext == L"it" || ext == L"s3m" || ext == L"mtm" || ext == L"mod" || ext == L"umx")
-            {
-                if (load_as_sample)
-                    return error_file(L, "Can't load as sample: ", lua_tostring(L, 1), false);
-                correct_ext = true;
-                index = _bass_loader.loadMusic(filename.c_str());
-            }
-        }
-
-        if (!correct_ext)
-            return error_file(L, "Unknown file type: ", lua_tostring(L, 1), false);
-        if (index == -1)
-            return error_file(L, "Can't open file: ", lua_tostring(L, 1), true);
-        lua_pushinteger(L, index);
+        int id = lua_tointeger(L, 1);
+        int volume = (n == 2) ? 100 : lua_tointeger(L, 2);
+        if (!_bass_loader.play(id, volume))
+             return error(L, _bass_loader.getLastError());
+        lua_pushboolean(L, 1);
         return 1;
     }
-    return error_invargs(L, load_as_sample ? "loadSample" : "load");
-}
-*/
-
-
-/*int lbass_load(lua_State *L)
-{
-    return lbass_load_ex(L, false);
-}
-
-int lbass_loadsample(lua_State *L)
-{
-    return lbass_load_ex(L, true);
-}
-
-int getIndex(lua_State *L)
-{
-    return (lua_gettop(L) == 1 && lua_isnumber(L, 1)) ? lua_tointeger(L, 1) : -1;
-}
-
-BassObject* getObject(lua_State *L)
-{
-    int index = getIndex(L);
-    return _bass_loader.get(index);
+    return error_invargs(L, "play");
 }
 
 int lbass_unload(lua_State *L)
 {
-    int index = getIndex(L);
-    if (index < 0)
-        return error_invargs(L, "unload");
-    bool result = _bass_loader.unload(index);
-    lua_pushboolean(L, result ? 1 : 0);
-    return 1;
-}*/
-
-int lbass_play(lua_State *L, const utf8* method, bool as_sample)
-{
-    int n = lua_gettop(L);
-    if ((n == 1 && lua_isstring(L, 1)) || 
-        (n == 2 && lua_isstring(L, 1) && lua_isnumber(L, 2)))
+    if (lua_gettop(L) == 1 && lua_isnumber(L, 1))
     {
-        int volume = (n == 2) ? lua_tointeger(L, 2) : 100;
-        if (volume >= 0 && volume <= 100)
+        if ( !_bass_loader.unload(lua_tointeger(L,1)) )
+            return error(L, _bass_loader.getLastError());        
+        lua_pushboolean(L, 1);
+        return 1;
+    }
+    return error_invargs(L, "unload");
+}
+
+int lbass_load(lua_State *L, const utf8* method, bool as_sample)
+{
+    if (lua_gettop(L) == 1 && lua_isstring(L, 1))
+    {
+        S2W file(lua_tostring(L, 1));
+        int id = _bass_loader.load(file, as_sample);
+        if (id == -1)
         {
-            S2W file(lua_tostring(L, 1));
-            if (!_bass_loader.play(file, volume, as_sample))
-            {
-                W2S errmsg(_bass_loader.getLastError());
-                return error(L, errmsg);
-            }
-            return 0;
+            W2S errmsg(_bass_loader.getLastError());
+            return error(L, errmsg);
         }
+        lua_pushinteger(L, id);
+        return 1;
     }
     return error_invargs(L, method);
 }
 
-int lbass_playSample(lua_State *L)
+int lbass_loadSample(lua_State *L)
 {
-   return lbass_play(L, "playSample", true);
+   return lbass_load(L, "loadSample", true);
 }
 
-int lbass_playStream(lua_State *L)
+int lbass_loadStream(lua_State *L)
 {
-    return lbass_play(L, "playStream", false);
+    return lbass_load(L, "loadStream", false);
 }
 
 int lbass_stop(lua_State *L)
 {
    int n = lua_gettop(L);
-   if (n == 1 && lua_isstring(L, 1))
+   if (n == 1 && lua_isnumber(L, 1))
    {
-       S2W file(lua_tostring(L, 1));
-       _bass_loader.stop(file);
-       return 0;
+       if (!_bass_loader.stop(lua_tointeger(L, 1)))
+            return error(L, _bass_loader.getLastError());
+       lua_pushboolean(L, 1);
+       return 1;
    }
    return error_invargs(L, "stop");
 }
@@ -354,7 +303,7 @@ int lbass_init(lua_State* L)
 
 int lbass_free(lua_State* L)
 {
-    if (lua_gettop(L) == 0)        
+    if (lua_gettop(L) == 0)
     {
         if (!_bass_loader.unloadBass())
             return error(L, _bass_loader.getLastError());
@@ -392,8 +341,10 @@ static const luaL_Reg lbass_methods[] =
 {
     { "init", lbass_init },
     { "free", lbass_free },
-    { "playSample", lbass_playSample },
-    { "playStream", lbass_playStream },
+    { "loadSample", lbass_loadSample },
+    { "loadStream", lbass_loadStream },
+    { "unload", lbass_unload },
+    { "play", lbass_play },    
     { "stop", lbass_stop },
     { "stopAll", lbass_stopAll },
     { "setVolume", lbass_setVolume },
