e27b12be8f79a63fd2f665becc1a9c9ea1974aa0
gmax <gm79@list.ru>
2016-02-16 15:57:01 +0300

    Улучшен плагин prompt.

--- plugins/prompt/prompt.cpp
+++ plugins/prompt/prompt.cpp
@@ -11,7 +11,8 @@ int get_description(lua_State *L)
 {
     luaT_pushwstring(L, L"Фильтрует(отбрасывает) prompt-строку, если она идет подряд без изменений.\r\n"
         L"Плагин позволяет не забивать окно мада строками prompt, если используются триггеры\r\n"
-        L"с командой #drop, а также различные фильтры, которые убирают целиком строку.");
+        L"с командой #drop, а также другие фильтры, которые убирают целиком всю строку.\r\n"
+        L"Плагин отбрасывает также и пустые строки между prompt.");
     return 1;
 }
 
@@ -21,53 +22,7 @@ int get_version(lua_State *L)
     return 1;
 }
 
-
-//std::wstring last_string;
 std::wstring last_prompt;
-int empty_counter = 0;
-/*
-void saveLastString(luaT_ViewData &vd)
-{
-    if (vd.isLast())
-        last_string.clear();
-    else
-    {
-        int strings_count = vd.size();
-        vd.select(strings_count);
-        if (vd.isDropped()) 
-          last_string.clear();
-        else {
-          std::wstring text;
-          vd.getText(&text);
-          last_string.append(text);
-        }
-    }
-}
-
-void clearLastString()
-{
-    last_string.clear();
-}
-*/
-int insertEmpty(luaT_ViewData &vd)
-{
-    int inserted = empty_counter;
-    while (empty_counter > 0)
-    {
-       vd.insertString();
-       empty_counter--;        
-    }
-    last_prompt.clear();
-    return inserted;
-}
-
-void clearEmpty()
-{
-    empty_counter = 0;
-    last_prompt.clear();
-}
-
-//------------------------------------------------------------------------
 void checkDoublePrompt(luaT_ViewData &vd)
 {
     int strings_count = vd.size();
@@ -75,154 +30,88 @@ void checkDoublePrompt(luaT_ViewData &vd)
         return;
 
     std::wstring text;
-    std::vector<int> not_empty;
-    int dropped = 0;
+    std::vector<int> todelete;
+
     for (int i = 1; i <= strings_count; ++i)
     {
         vd.select(i);
-        if (vd.isDropped())
-         {
-             dropped++;
-             continue;
-        }
+        if (vd.isDropped()) { continue; }
         if (vd.isGameCmd())
-            { not_empty.push_back(i); continue; }
-        if ((i == 1 && vd.isFirst()) || 
-           (i == strings_count && !vd.isLast()))
-            { not_empty.push_back(i); continue; }
+            { todelete.clear(); last_prompt.clear(); continue; }
         vd.getText(&text);
-        if (!text.empty()) 
-            { not_empty.push_back(i); }
-    }
-
-    if (not_empty.empty())                // all vd strings are empty
-    {
-        // count empty strings
-        empty_counter += (strings_count - dropped);
-        // delete all strings from vd
-        for (int j=strings_count; j >= 1; --j)
+        if (text.empty())
         {
-            vd.select(j);
-            vd.deleteString();
+            if (!last_prompt.empty())
+                todelete.push_back(i);
+            continue;
         }
-        return;
-    }
-
-    // check not empty
-    for (int i=0,e=not_empty.size();i<e;++i)
-    {
-        vd.select(not_empty[i]);
         if (vd.isPrompt())
         {
             if (last_prompt.empty())
+                vd.getPrompt(&last_prompt);
+            else
             {
-                vd.getPrompt(&last_prompt);            
+                vd.getPrompt(&text);
+                if (text == last_prompt) 
+                    todelete.push_back(i);
+                else
+                {
+                    last_prompt = text;
+                    todelete.clear();
+                }
             }
-            
-
+            continue;
         }
+        todelete.clear();
     }
+/*
+        static int k = 0;
+        for (int i=1;i<=strings_count; ++i)
+        {
+            vd.select(i);
+            vd.getText(&text);
+            OutputDebugString(L"[");
+            OutputDebugString(text.c_str());
+            OutputDebugString(L"]");
 
-
-
-    /*
-    for (int i = 1; i <= strings_count; ++i)
+            bool todel = false;
+            for (int j=0,e=todelete.size();j<e; ++j)
+            {
+                if (todelete[j] == i) { todel = true; break; }
+            }
+            if (todel)
+                 OutputDebugString(L" --- deleted");
+            if (vd.isDropped())
+                 OutputDebugString(L" --- dropped");
+            OutputDebugString(L" \r\n");
+        }
+        wchar_t buffer[16];
+        _itow(k++, buffer, 10);
+        OutputDebugString(buffer);
+        OutputDebugString(L" ############\r\n");
+*/
+    if (!todelete.empty())
     {
-        vd.select(i);
-        if (vd.isDropped())
-            continue;
-
-        if (vd.isGameCmd())
+        if (todelete.size() == strings_count)
         {
-            if (i == 1 && vd.isFirst())
-            {
-              clearEmpty();
-              continue;
-            }
-            int count = insertEmpty(vd);
-            strings_count += count;
-            i = i + count;
-            continue;
-        }       
-
-        if (i == strings_count && !vd.isLast())
-        {
-            break;
-        }
-
-        if (i == 1 && vd.isFirst()) {
-           vd.getText(&text);
-           last_string.append(text);
-           text.swap(last_string);     // text with first full string
-           last_string.clear();
-        } else {
-           vd.getText(&text);
-        }
-        if (text.empty())
-        {
-            empty_counter++;
-            vd.deleteString();
-            strings_count = vd.size();
-            i = i - 1;
-            continue;
-        }
-
-        if (!vd.isPrompt())
-        {
-            int count = insertEmpty(vd);
-            strings_count = vd.size();
-            i = i + count;
-            continue;
-        }
-        if (last_prompt.empty())
-        {
-            vd.getPrompt(&last_prompt);
-
-            continue;
-        }
-
-        std::wstring prompt;
-        vd.getPrompt(&prompt);
-        if (prompt == last_prompt)
-        {
-            empty.push_back(i);
-            for (int j = empty.size() - 1; j >= 0; --j)
-            {
-                vd.select(empty[j]);
-                vd.deleteString();
-            }
-            strings_count = vd.size();           
-            if (!strings_count)
-                break;
-            i = empty[0];
-            empty.clear();
+            vd.deleteAllStrings();        
         }
         else
         {
-            last_prompt.clear();
-            empty.clear();
+            for (int j = todelete.size() - 1; j >= 0; --j)
+            {
+                vd.select(todelete[j]);
+                vd.deleteString();
+            }
         }
     }
-
-    if (vd.isLast())
-        last_string.clear();
-    else
-    {
-        vd.select(strings_count);
-        if (vd.isDropped()) 
-          last_string.clear();
-        else {
-          vd.getText(&text);
-          last_string.append(text);
-        }
-    }*/
 }
 
 int afterstr(lua_State *L)
 {
     if (!luaT_check(L, 2, LUA_TNUMBER, LUAT_VIEWDATA))
         return 0;
-    if (lua_tointeger(L, 1) != 0) // view number
+    if (lua_tointeger(L, 1) != 0) // view number, only for main window
         return 0;
     luaT_ViewData vd;
     vd.init(L, luaT_toobject(L, 2));
@@ -232,8 +121,7 @@ int afterstr(lua_State *L)
 
 int disconnect(lua_State *L)
 {
-    clearEmpty();
-    //clearLastString();
+    last_prompt.clear();
     return 0;
 }
 
