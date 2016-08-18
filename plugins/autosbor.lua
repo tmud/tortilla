75f3154b621d783e5b107e84e6e2f0c5f83effb5
gmax <gm79@list.ru>
2016-08-18 19:10:42 +0300

    Дописан автосбор.

--- plugins/autosbor.lua
+++ plugins/autosbor.lua
@@ -1,9 +1,12 @@
 ﻿-- Символы вкл/выкл, отображаемые в общей таблице сбора
 local sym_yes = '+'
-local sym_no = '-'
+local sym_no = ' '
 
 -- цвет заголовка окна общего состояния сбора
-local title_color = 'rgb240,120,80'
+local title_color = 'rgb240,120,80 b rgb90,40,20'
+
+-- цвет текста для тега с качеством ресурса
+local teg_color = 'light red b blue'
 
 -- триггеры на ресурсы
 ------------------------------------------------------------------------------------
@@ -21,34 +24,152 @@ local vetki = {
 local griby = {
   ingr = 'грибы',
   trigger = 'Гриб (%1) растет здесь.',
+  q6 = { 'волнушка', 'говорушка' },
+  q5 = { 'масленок', 'подберезовик', 'рыжик', 'тонкая свинушка' },
+  q4 = { 'дождевик', 'едкая сыроежка', 'печерица' },
+  q3 = { 'белый груздь', 'красный мухомор', 'красный опенок', 'подосиновик', 'черный груздь' },
+  q2 = { 'бледная поганка', 'боровик', 'жгучая сыроежка', 'подвишенник' },
+  q1 = { 'белый мухомор', 'желтый ежевик', 'навозник', 'пятнистый мухомор', 'скрипица'}
 }
 local griby2 = {
   ingr = 'грибы',
   trigger = '%1 растут на пеньке.',
   q6 = { 'опята' }
 }
+local zidkosti = {
+  ingr = 'жидкости',
+  trigger = 'Лужица %1 разлита у ваших ног.',
+  q6 = { 'дождевой воды', 'прокисшего пива', 'ржаного кваса' },
+  q5 = { 'вина', 'ключевой воды', 'сметаны' },
+  q4 = { 'медовухи', 'простокваши', 'самогона' },
+  q3 = { 'кислого молока', 'клюквеного киселя', 'сбитня' },
+  q2 = { 'масла', 'меда' },
+  q1 = { 'дегтя', 'конопляного масла', 'крови', 'ртути' }
+}
+local krupy = {
+  ingr = 'крупы',
+  trigger = 'Немного %1 просыпано здесь.',
+  q6 = { 'манки', 'отрубей' },
+  q5 = {},
+  q4 = { 'овса', 'ячменя' },
+  q3 = { 'гороховой крупы', 'гречневой ядрицы' },
+  q2 = { 'проса', 'толокна' },
+  q1 = { 'гречки', 'ячневой крупы' }
+}
+local metall = {
+  ingr = 'металлы',
+  trigger = 'Маленький кусочек %1 валяется в пыли.',
+  q6 = { 'олова' },
+  q5 = {},
+  q4 = {},
+  q3 = { 'латуни', 'свинца' },
+  q2 = { 'мышьяка', 'серебра', 'чугуна' },
+  q1 = { 'платины' }
+}
+local metall2 = {
+  ingr = 'металлы',
+  trigger = '%1 еле заметен в грязи.',
+  q1 = { 'золотой самородок' }
+}
+local mineraly = {
+  ingr = 'минералы',
+  trigger = 'Осколок %1 наполовину утоплен в почву.',
+  q6 = { 'кремня' },
+  q5 = {},
+  q4 = { 'базальта' },
+  q3 = { 'мрамора', 'халцедона', 'хризопраза' },
+  q2 = { 'опала', 'турмалина' },
+  q1 = { 'александрита', 'алмаза', 'граната', 'изумруда', 'рубина' }
+}
+local ovoshi = {
+  ingr = 'овощи',
+  trigger = 'Здесь лежит %1.',
+  q6 = { 'морковь', 'редис' },
+  q5 = { 'редька' },
+  q4 = { 'горох', 'кабачок', 'репа', 'фасоль' },
+  q3 = { 'петрушка', 'сладкий перец', 'укроп' },
+  q2 = { 'баклажан', 'красный перец', 'цветная капуста' },
+  q1 = { 'лук-порей', 'ревень', 'тыква' }
+}
+local posuda = {
+  ingr = 'посуда',
+  trigger = 'У ваших ног лежит %1.',
+  q6 = { 'глиняная плошка', 'оловянная чашка' },
+  q5 = {},
+  q4 = { 'большой горшок', 'железная ступка' },
+  q3 = { 'железный котел', 'маленький котелок' },
+  q2 = { 'бронзовый котел', 'железный чан', 'медный котел', 'медный чан', 'оловянный котел' },
+  q1 = { 'огромный медный котел', 'огромный серебряный котел', 'серебряный тигель' }
+}
+local tvari = {
+  ingr = 'твари',
+  trigger = 'Вы заметили %1.',
+  q6 = { 'птенца воробушка' },
+  q5 = { 'клопа-вонючку', 'крошечную змейку', 'ручейника' },
+  q4 = { 'клопа-гладыша', 'муравьишку', 'светлячка' },
+  q3 = { 'желтого жучка', 'жужелицу', 'лягушонка', 'уховертку' },
+  q2 = { 'дохлую водомерку', 'медведку', 'синего жучка', 'слепня' },
+  q1 = { 'землеройку', 'красного жучка', 'мокрицу', 'сороконожку' }
+}
+local tvari2 = {
+  ingr = 'твари',
+  trigger = '%1 извивается здесь.',
+  q3 = { 'длинный червяк' }
+}
+local tvari3 = {
+  ingr = 'твари',
+  trigger = '%1 катит свой шар мимо.',
+  q1 = { 'навозный жук' }
+}
+local tvari4 = {
+  ingr = 'твари',
+  trigger = '%1 шевелит длинными усами.',
+  q2 = { 'рыжий таракан' }
+}
+local travy = {
+  ingr = 'трава',
+  trigger = 'Среди разнотравья вы заметили %1.',
+  q6 = { 'авран', 'ромашку' },
+  q5 = { 'дрок', 'льнянку', 'мяту', 'чагу' },
+  q4 = { 'барвинок', 'девясил', 'купальницу', 'солодку', 'шалфей' },
+  q3 = { 'арнику', 'борец', 'валериану', 'крапиву', 'прострел', 'толокнянку', 'тысячелистник', 'чистец' },
+  q2 = { 'белену', 'василистник', 'душицу', 'зверобой', 'крушину', 'молочай', 'обвойник', 'плаун', 'чистотел' },
+  q1 = { 'аир', 'бессмертник', 'вех ядовитый', 'горец перечный', 'спорынью', 'чабрец', 'ясенец' }
+}
+local yagody = {
+  ingr = 'ягоды',
+  trigger = 'Приглядевшись, вы видите ягоду %1.',
+  q6 = { 'бирючины', 'брусники', 'бычьего языка', 'дикого винограда', 'шиповника' },
+  q5 = { 'вишни', 'голубики', 'клещевины', 'клюквы', 'метельника', 'тиса' },
+  q4 = { 'барбариса', 'бересклета', 'малины', 'остролиста', 'черной смородины' },
+  q3 = { 'белладонны', 'боярышника', 'ежевики', 'жимолости', 'клубники', 'красной жимолости', 'можжевельника', 'паслена', 'черники' },
+  q2 = { 'белой смородины', 'волчьего лыка', 'дрока', 'дурмана', 'земляники', 'ирги', 'красной смородины', 'ландыша', 'сизой ежевики' },
+  q1 = { 'белой малины', 'брионии', 'золотой акации', 'кизила', 'костяники', 'омелы', 'падуба', 'черноплодной рябины' }
+}
+
 -- общий список всех триггеров
-local triggers = { vetki } --, griby, griby2 }
+local triggers = { vetki, griby, griby2, zidkosti, krupy, metall, metall2, mineraly, ovoshi, posuda, tvari, tvari2, tvari3, tvari4, travy, yagody }
 ------------------------------------------------------------------------------------
 -- качества в падежах
 local level_s = { 'наилучшую', 'отличную', 'хорошую', 'среднюю', 'посрдественную', 'худшую' }
 local level_m = { 'наилучшие', 'отличные', 'хорошие', 'средние', 'посрдественные', 'худшие' }
 local male = { 'наилучший', 'отличный', 'хороший', 'средний', 'посредственный', 'худший' }
 local female = { 'наилучшая', 'отличная', 'хорошая', 'средняя', 'посредственная', 'худшая' }
+local neutral = { 'наилучшее', 'отличное', 'хорошее', 'среднее', 'посрдественное', 'худшее' }
 
 -- мн.число, ед.число(вин.пад), ед.число(им.падеж), пол, цвет
 local ingrs = {
-  { 'ветки', 'ветку', 'ветка', gender=female, level=level_m, color='light green' },
-  { 'грибы', 'гриб', 'гриб', gender=male, level=level_m, color='light red' },
-  { 'жидкости', 'жидкость', 'жидкость', gender=female, level=level_m, color='light magenta' },
-  { 'крупы', 'крупу', 'крупа', gender=female, level=level_m, color='light cyan' },
-  { 'металлы', 'металл', 'металл', gender=male, level=level_m, color='light green' },
-  { 'минералы', 'минерал', 'минерал', gender=male, level=level_m, color='light red' },
-  { 'овощи', 'овощ', 'овощ', gender=male, level=level_m, color='light magenta' },
-  { 'посуда', 'посуду', 'посуда', gender=female, level=level_s, color='light cyan' },
-  { 'твари', 'тварь', 'тварь', gender=female, level=level_m, color='light green' },
-  { 'трава', 'траву', 'трава', gender=female, level=level_s, color='light red' },
-  { 'ягоды', 'ягоду', 'ягода', gender=female, level=level_m, color='light magenta' }
+  { 'ветки', 'ветку', 'ветка', gender=female, level=level_m, color='b blue' },
+  { 'грибы', 'гриб', 'гриб', gender=male, level=level_m, color='' },
+  { 'жидкости', 'жидкость', 'жидкость', gender=female, level=level_m, color='b blue' },
+  { 'крупы', 'крупу', 'крупа', gender=female, level=level_m, color='' },
+  { 'металлы', 'металл', 'металл', gender=male, level=level_m, color='b blue' },
+  { 'минералы', 'минерал', 'минерал', gender=male, level=level_m, color='' },
+  { 'овощи', 'овощ', 'овощ', gender=male, level=level_m, color='b blue' },
+  { 'посуда', 'посуду', 'посуда', gender=female, level=level_s, color='' },
+  { 'твари', 'тварь', 'тварь', gender=female, level=level_m, color='b blue' },
+  { 'трава', 'траву', 'трава', gender=female, level=level_s, color='' },
+  { 'ягоды', 'ягоду', 'ягода', gender=female, level=level_m, color='b blue' }
 }
 
 local function f(x) 
@@ -58,22 +179,45 @@ local function print(x)
   _G.print('[автосбор]: '..x)
 end
 local function printc(x, ...)
-  if not x then _G.print(...) return end
+  if not x or x =='' then _G.print(...) return end
   _G.print(x, ...)
 end
 
+local teg_text_color, teg_bkg_color
+local delimeter
+local function updateColors()
+  delimeter = props.cmdSeparator()
+  if type(teg_color) == 'string' then
+    teg_text_color, teg_bkg_color = translateColors( teg_color, props.paletteColor(7), props.backgroundColor() )
+  end
+  if not teg_text_color then
+    teg_text_color = props.paletteColor(7)
+    teg_bkg_color = props.backgroundColor()
+  end
+end
+
 -- добавление тега в строку с ресурсом
-local function addteg(vd, ingr, quality)
+local function addteg(vd, ingr, quality, flag)
   local idx = vd:blocks()+1
   vd:setBlocksCount(idx)
-  vd:setBlockText(idx, ' [test]')
-  --vd:set(idx, 'textcolor', props.paletteColor(5))
-  vd:set(idx-1, 'textcolor', 5)
+  local t = vd:getBlockText(idx-1)
+  vd:setBlockText(idx-1, t..' ')
+  local prefix = flag and '' or '-'
+  vd:setBlockText(idx, '['..prefix..ingr[3]..'/'..neutral[quality]..']')
+  vd:set(idx, 'exttextcolor', teg_text_color)
+  vd:set(idx, 'extbkgcolor', teg_bkg_color)
 end
+
+local sbor = false
+local sumka
 
 -- запуск команды на сбор ингредиента
 local function collect(name, ingr, quality)
-  print('сбор '..name..' !')
+  local cmd = 'вз все.'..ingr[3]
+  if sumka then
+    cmd = cmd..delimeter..'пол все.'..ingr[3]..' '..sumka
+  end
+  runCommand(cmd)
 end
 
 -- обработка триггера на ингредиент
@@ -81,6 +225,7 @@ local function ingr_trigger(vd, ingr, qt)
   vd:select(1)
   local name = vd:getParameter(1)
   if not name then return end
+  name = name:lower()
   -- определяем качество ресурса
   local quality
   for i=1,6 do
@@ -92,21 +237,22 @@ local function ingr_trigger(vd, ingr, qt)
     end
   end
   if not quality then return end
-  -- добавляем тег на ресурс
-  addteg(vd, ingr, quality)
   -- проверяем что мы вообще собираем этот ресурс и собираем нужного качества
-  if not ingr.state or not ingr.val[quality] then return end
-  collect(name, ingr, quality)
+  local sbor_flag = ingr.state and ingr.val[quality]
+  -- добавляем тег на ресурс
+  addteg(vd, ingr, quality, sbor_flag)
+  -- проверяем что мы вообще собираем этот ресурс и собираем нужного качества
+  if sbor_flag then collect(name, ingr, quality) end
 end
 
-local sbor = false
 local cmdrxp, cmdrxp2, cmdrxp3, decl
 local autosbor = {}
 function autosbor.init()
   if not extra or not extra.declension then
     terminate('Для работы плагина нужен модуль extra.declension')
   end
-  decl = extra.declension() 
+  decl = extra.declension()
+  decl:add('_сумка')
   sbor = false
   for _,t in ipairs(ingrs) do
     decl:add(t[1])
@@ -139,6 +285,8 @@ function autosbor.init()
       j=j+1
     end
     sbor = read(vars, 'on')
+    sumka = vars['sumka']
+    if sumka == '' then sumka = nil end
   end
   cmdrxp = createPcre('сбор(.*)')
   cmdrxp2 = createPcre('^(_[^+-]+)?([+-])$')
@@ -151,16 +299,18 @@ function autosbor.init()
       if t.ingr == i[1] then ingr = i break end
     end
     if not ingr then log('Не найден игредиент: '..tostring(t.ingr)) goto next end
-    local tr = createTrigger(t.trigger, function(vd) ingr_trigger(vd, ingr, t) end)
+    local tr = createTrigger(t.trigger, function(vd) if sbor then ingr_trigger(vd, ingr, t) end end)
     if not tr then log('Не создан триггер: '..tostring(t.trigger)) end
     ::next::
   end
+  updateColors()
 end
 
 function autosbor.release()
   -- сохраняем настройки сбора
   local vars = {}
   vars.on = sbor
+  vars.sumka = sumka and sumka or ''
   for idx,t in ipairs(ingrs) do
     local s = {}
     s.on = t.state
@@ -169,6 +319,11 @@ function autosbor.release()
     vars['i'..idx] = s
   end
   saveTable(vars, getProfile()..".lua")
+end
+
+-- обновление цвета после настроек
+function autosbor.propsupdated()
+  updateColors()
 end
 
 -- работа с падежами
@@ -195,6 +350,8 @@ end
 local function runcmd(p)
   if p == '' then
     -- общее состояние сбора
+    if not sumka then print('Собирается все в инвентарь.')
+    else print('Собирается все в сумку: '..sumka) end
     printc(title_color, '| '..f(sbor), '| автосбор  |наилуч(1)|отличн(2)|хороше(3)|средне(4)|посред(5)|худшее(6)|')
     for _,t in ipairs(ingrs) do
       local flags = ''; for j=1,6 do flags=flags..'    '..f(t.val[j])..'    |' end
@@ -269,10 +426,27 @@ end
 function autosbor.gamecmd(t)
   if cmdrxp:find(t[1]) then
     local tl = #t
-    if tl == 1 or (tl == 2 and t[2]:trim() == '') then
-      local cmd = cmdrxp:get(1)
-      if runcmd(cmd:trim()) then return end
+    if tl > 2 then print('НЕПРАВИЛЬНАЯ КОМАНДА') return end
+    local p
+    if tl == 2 then
+      p = t[2]:trim()
+      if p == '' then p = nil end
     end
+    local cmd = cmdrxp:get(1)
+    cmd = cmd:trim()
+    local t = decl:find(cmd)
+    if t and t[1]=='_сумка' and cmd:len()>1 then
+      if not p then
+        print('ВСЕ БУДЕТ СОБИРАТЬСЯ В ИНВЕНТАРЬ.')
+        sumka = nil
+      else
+        print('ВСЕ БУДЕТ СОБИРАТЬСЯ В СУМКУ: '..p)
+        sumka = p
+      end
+      return
+    end
+    if p then print('НЕПРАВИЛЬНАЯ КОМАНДА') return end
+    if runcmd(cmd) then return end
   end
   return t
 end
@@ -286,6 +460,7 @@ function autosbor.description()
   'минералы, овощи, посуду, отвары, ягоды) различного качества.',
   'Команда: сбор[_что][качество]+-',
   'Вывод текущего статуса автосбора: сбор',
+  'Задать, сбросить сумку для сбора: cбор_сумка мешок, сбор_сумка',
   'Включить или выключить автосбор полностью: сбор+, сбор-',
   'Сбор отдельного ресурса: сбор_трава+, сбор_ветки-',
   'Сбор отдельного ресурса и качества: сбор_ягоды3+, сбор_грибы2-',
