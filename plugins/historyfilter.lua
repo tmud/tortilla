495bfa672f8f4402ea2eea22a6607146c36ae15c
gmax <gm79@list.ru>
2016-10-05 13:18:36 +0300

    улучшен плагин historyfilter.

--- plugins/historyfilter.lua
+++ plugins/historyfilter.lua
@@ -1,33 +1,41 @@
 ﻿-- historyfilter
 -- Плагин Tortilla mud client
--- Список команд, которые нужно блокировать
-local cmds_list_for_block = {'с','ю','з','в','вв','вн' }
+local cmds_list_for_block = {}
 
 local historyfilter = {}
 function historyfilter.name() 
-    return 'Фильтр истории команд'
+  return 'Фильтр истории команд'
 end
 
 function historyfilter.description()
-local cmds = ''
-for i = 1,#cmds_list_for_block do
-if i ~= 1 then cmds = cmds..', ' end
-cmds = cmds..cmds_list_for_block[i]
-end
-return 'Плагин блокирует попадание отдельных команд в историю.\r\n\z
-Настройки задаются прямо в файле плагина plugins/historyfilter.lua\r\n\z
-Текущие настройки фильтра: '..cmds
+  local desc = {
+  'Плагин блокирует попадание отдельных команд в историю.',
+  'Настройки задаются в файле: '..getPath('config.lua')
+  }
+  return table.concat(desc, '\r\n')
 end
 
 function historyfilter.version()
-    return '-'
+  return '1.0'
+end
+
+function historyfilter.init()
+  local t = loadTable('config.lua')
+  if not t then
+    terminate('Нет файла настроек '..getPath('config.lua')..', со списком команд.')
+  end
+  if not t.cmdlist or type(t.cmdlist) ~= 'table' then
+    terminate('Ошибка в настройках '..getPath('config.lua')..', нет списка команд cmdlist.')
+  end
+  cmds_list_for_block = {}
+  for _,cmd in pairs(t.cmdlist) do
+    cmds_list_for_block[cmd] = true
+  end
 end
 
 function historyfilter.historycmd(cmd)
-for i = 1,#cmds_list_for_block do
-  if cmds_list_for_block[i] == cmd then return false end
-end
-return true
+  if cmds_list_for_block[cmd] then return false end
+  return true
 end
 
 return historyfilter
