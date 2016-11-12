97bf9a2e665604d0e978486c4ad23653352f4a8d
gmax <gm79@list.ru>
2016-11-13 01:17:28 +0300

    Расширен плагин spit за счет textdb.

--- plugins/spit.lua
+++ plugins/spit.lua
@@ -7,19 +7,27 @@ function spit.name()
 end
 
 function spit.description()
+  local p = props.cmdPrefix()
+  local c = p..'spitdb '
   local s  = {
   'Плагин читает файл и отправляет его построчно в мад.',
-  '#spit <файл> [строка для подстановки] [номера строк]',
   'Плагин работает с файлами в кодировке win1251.',
+  p..'spit <файл> [строка для подстановки] [номера строк]',
   'Можно не указывать строку для подстановки и/или номера строк.',
   'Если в строке для подстановки используется %0, то строки из файла подставляютca вместо %0.',
-  'Номера строк можно указывать списком или диапазонами, например: 1 | 1,2,5 | 1-6'
+  'Номера строк можно указывать списком или диапазонами, например: 1 | 1,2,5 | 1-6',
+  c..'read <фaйл> <номер строки> <имя переменной> - читает строку в переменную,',
+  'имя переменной нужно указывать без $. Значение используется через $var.',
+  c..'write <фaйл> <номер строки> <текст> - записывает строку в нужную позицию',
+  'в файле. Запись кэшируется. Сброс данных на диск происходит при закрытии плагина или',
+  'клиента, а также при выполнении команды flush.',
+  c..'flush <фaйл> - сбрасывает кэш(все что не записано) на диск.'
   }
   return table.concat(s, '\r\n')
 end
 
 function spit.version()
-  return '1.0'
+  return '1.01'
 end
 
 local function print(s)
@@ -28,10 +36,14 @@ end
 
 local number, diapozon, templ
 function spit.init()
-  if not system or not system.loadTextFile then
-    terminate('Для работы плагина нужен модуль system.loadTextFile')
+  if not checkVersion(1,0) then
+    terminate('Для работы плагина требуется клиент версии 1.0+')
+  end
+  if not system then
+    terminate('Для работы плагина нужен модуль system.')
   end
   addCommand("spit")
+  addCommand('spitdb')
   number = createPcre('^[0-9]+$')
   diapozon = createPcre('^([0-9]+)-([0-9]+)$')
   templ = createPcre('%0')
@@ -92,8 +104,12 @@ local function send(file, template, lines)
       if i ~= last then out[#out+1] = '' end
     end
   else
-    out[1] = template..' '
-    out[2] = ''
+    if template == '' then
+      out[1] = ''
+    else
+      out[1] = template..' '
+      out[2] = ''
+    end
   end
   if lines then
     for _,i in ipairs(lines) do
@@ -109,21 +125,78 @@ local function send(file, template, lines)
 end
 
 local function printhelp()
-  print('Формат команды: '..props.cmdPrefix()..'spit <файл> [строка для подстановки] [номера строк]')
+  local p=props.cmdPrefix()
+  print('Формат команды: ')
+  print(p..'spit <файл> [строка для подстановки] [номера строк]')
+  print(p..'spitdb read|write|flush <файл> <номер строки> <перем|текст>')
 end
 
-function spit.syscmd(t)
-  if t[1] ~= 'spit' then return t end
-  if #t == 1 then printhelp() return end
-  local f = system.loadTextFile(t[2])
+local function writeFileTable(filename, t)
+  return system.saveTextFile(filename, t)
+end
+
+local files = {}
+local function writeFile(filename)
+  local f = files[filename]
+  if not f then return end
+  writeFileTable(filename, f)
+end
+
+local function loadFile(filename, hideerr)
+  local f = files[filename]
   if not f then
-    print('Не удалось прочитать файл: '..t[2])
-    return
+    f = system.loadTextFile(filename)
+    if not f then
+      if not hideerr then
+        print('[textdb] Файл не найден: '..filename)
+      end
+      return
+    end
+    files[filename] = f
   end
-  if #t == 2 then
-    send(f, '')
-    return
+  return f
+end
+
+function spit.release()
+  for filename, t in pairs(files) do
+    writeFileTable(filename, t)
   end
+end
+
+local function read(t)
+  if #t ~= 5 then return false end
+  local index = tonumber(t[4])
+  if not index then return false end
+  local f = loadFile(t[3])
+  if not f then return true end
+  if index >= 1 and index <= #f then
+    vars:replace(t[5], f[index])
+  else
+    vars:replace(t[5], '')
+  end
+  return true
+end
+
+local function write(t)
+  if #t ~= 5 then return false end
+  local index = tonumber(t[4])
+  if not index then return false end
+  local f = loadFile(t[3], true)
+  if not f then f = {} files[t[3]] = f end
+  if index >= 1 then
+    local from = #f+1
+    for i=from,index-1 do f[i] = '' end
+    f[index] = t[5]
+    return true
+  end
+  return false
+end
+
+local function spitcmd(t)
+  if #t == 1 then printhelp() return end
+  local f = loadFile(t[2])
+  if not f then return end
+  if #t == 2 then send(f, '') return end
   if #t == 3 then
     local p = t[3]
     if p:find('%0') then
@@ -146,6 +219,22 @@ function spit.syscmd(t)
     end
   end
   printhelp()
+end
+
+function spit.syscmd(t)
+  if t[1] == 'spit' then
+    spitcmd(t)
+    return
+  end
+  if t[1] == 'spitdb' then
+    if #t < 3 then printhelp() return end
+    if t[2] == 'flush' then writeFile(t[3]) return end
+    if t[2] == 'read' and read(t) then return end
+    if t[2] == 'write' and write(t) then return end
+    printhelp()
+    return
+  end
+  return t
 end
 
 return spit
