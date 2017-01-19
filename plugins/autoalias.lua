8bf67d310aeed45311770245bc74c2e15f2d7be3
gmax <gm79@list.ru>
2017-01-19 12:13:29 +0300

    Публичная версия 1.0beta7.

--- plugins/autoalias.lua
+++ plugins/autoalias.lua
@@ -8,20 +8,21 @@ end
 
 function autoalias.description()
   local s = { 'Плагин пробует подобрать макрос(алиас) по набранной команде,',
-  'учитывая, что по ошибке могут быть набраны опечатки в начале команды.',
+  'учитывая, что по ошибке могут быть набраны лишние пробелы в начале команды.',
   'Обрабатываются только команды, введенные в командной строке клиента.'
   }
   return table.concat(s, '\r\n')
 end
 
 function autoalias.version()
-  return '1.0'
+  return '1.01'
 end
 
 function autoalias.barcmd(t)
   if #t == 0 then return t end
   if #t == 1 and t[1] == '' then return t end
   local c = t[1]
+  if c:substr(1, 1) ~= ' ' then return t end
   for i=1,aliases:size() do
     aliases:select(i)
     local group = aliases:get('group')
@@ -30,18 +31,23 @@ function autoalias.barcmd(t)
       if v == '1' then
         local alias = aliases:get('key')
         local pos = c:find(alias)
-        if pos then
+        if pos and pos > 0 then
           local len1 = pos+alias:len()
-          local len2 = c:len()+1
-          if len1 == len2 or c:substr(len1, 1) == ' ' then 
-            local newcmd = alias..c:substr(len1, len2-len1)
-            t[1] = newcmd
-            return t
+          if c:substr(len1, 1) == ' ' then
+            local prefix = c:substr(pos-1)
+            prefix = prefix:trim()
+            if prefix == '' then
+              local len2 = c:len()+1
+              local newcmd = alias..c:substr(len1, len2-len1)
+              t[1] = newcmd
+              break
+            end
           end
         end
       end
     end
   end
+  return t
 end
 
 return autoalias
