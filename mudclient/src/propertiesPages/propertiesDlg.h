6879f68d2e9f85fc961ea49b2088d740dc91c8ed
gmax <gm79@list.ru>
2017-07-21 13:51:47 +0300

    Изменилось окно подтверждения сохранения изменений в шаблоне триггеров и пр.

--- mudclient/src/propertiesPages/propertiesDlg.h
+++ mudclient/src/propertiesPages/propertiesDlg.h
@@ -241,9 +241,13 @@ private:
 	LRESULT OnCloseCmd(WORD, WORD wID, HWND, BOOL&)
 	{
         if (wID == IDOK) {
-          if (checkOrUpdate(true) && msgBox(m_hWnd, L"Были изменены шаблоны некоторых элементов, но не сохранены. Сохранить их?", MB_YESNO|MB_ICONQUESTION)==IDYES)
+          if (checkOrUpdate(true))
           {
-              checkOrUpdate(false);
+              int result = msgBox(m_hWnd, L"Шаблон текущего элемента был изменен, но не сохранен. Сохранить его?", MB_YESNOCANCEL|MB_ICONQUESTION);
+              if (result == IDCANCEL)
+                  return 0;
+              if (result == IDYES)
+                checkOrUpdate(false);
           }
         }
 		EndDialog(wID);
