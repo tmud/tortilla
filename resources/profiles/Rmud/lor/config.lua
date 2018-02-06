cdd5e28bf7d4da01c4ab16a0c3c79173a8ddbc8c
gmax <gm79@list.ru>
2018-02-06 14:25:06 +0300

    Плагин-пример и конфиг лора для RMUD

---
+++ resources/profiles/Rmud/lor/config.lua
@@ -0,0 +1,14 @@
+﻿key = "^Вы получили некоторую информацию."
+
+check = function()
+  local r1 = createPcre('^Название "(.*)",')
+  local r2 = createPcre('^Вы получили некоторую информацию.')
+  return function(vs)
+    local s = vs:getText()
+    if r1:find(s) then return true, r1:get(1) end
+    if r2:find(s) then return false end
+    return true
+  end
+end
+
+perpage = 20
