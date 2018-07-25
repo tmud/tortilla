76a4599510c9d725f4c16e5b1d747c1425f6e59e
gmax <gm79@list.ru>
2018-07-25 14:21:17 +0300

    мелкие правки

--- mudclient/src/stdafx.h
+++ mudclient/src/stdafx.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define TORTILLA_VERSION L"1.04beta"
+#define TORTILLA_VERSION L"1.05"
 #define TORTILLA_VERSION_MAJOR 1
-#define TORTILLA_VERSION_MINOR 4
+#define TORTILLA_VERSION_MINOR 5
 
 #ifndef _UNICODE
 #error("Support only unicode version!")
