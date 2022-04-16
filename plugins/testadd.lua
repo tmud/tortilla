cdea0f3409e9053763aad79fa01c719bce4dc579
gmax79 <gm79@list.ru>
2022-04-16 23:09:27 +0300

    тест подсветок

---
+++ plugins/testadd.lua
@@ -0,0 +1,32 @@
+﻿-- testadd
+-- Плагин для Tortilla mud client
+
+local testadd = {}
+function testadd.name()
+  return 'Тесты добавления подсветок и прочего из луа'
+end
+function testadd.version()
+  return '-'
+end
+function testadd.description()
+  return "Плагин для тестирования добавления разных элементов"
+end
+
+
+function testadd.init()
+  addCommand('testadd')
+end
+
+function testadd.syscmd(t)
+  if t[1] ~= 'testadd' then
+   return t
+  end
+
+  local x = highlights:add ("стоите", "rgb192,192,192 b rgb20,40,90") 
+  log(x)
+
+  return nil
+end
+
+
+return testadd
